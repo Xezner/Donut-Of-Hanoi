7a9e36b64cd69d5af7d35a4c01cd637e3cce95a9
Prencyl <prencylabueg@gmail.com>
2023-07-15 03:15:52 +0800

    interact event system: added following features: - event system for inteaction - pick up system - drop system - selected toggler - updated diskobjects

--- Assets/Scripts/KitchenObject/CounterObject.cs
+++ Assets/Scripts/KitchenObject/CounterObject.cs
@@ -6,16 +6,17 @@ using UnityEngine;
 public class CounterObject : MonoBehaviour
 {
     [SerializeField] DiskHolder _diskHolder;
-    [SerializeField] GameObject _selectedOverlay;
     [SerializeField] private bool _isDeliveryCounter;
-    // Start is called before the first frame update
+
     private void Start()
     {
-        
+        if (_isDeliveryCounter)
+        {
+            InteractionManager.Instance.OnDiskStackChange += Instance_OnDiskStackChange;
+        }
     }
 
-    // Update is called once per frame
-    private void Update()
+    private void Instance_OnDiskStackChange(object sender, InteractionManager.OnDiskStackChangeEventArgs diskStackChangeEvent)
     {
         if(_isDeliveryCounter)
         {
@@ -23,23 +24,33 @@ public class CounterObject : MonoBehaviour
         }
     }
 
-
     private void CheckDiskHolderStack()
     {
         bool isStackEqual = _diskHolder.GetDiskStack().SequenceEqual(DiskSpawnManager.Instance.GetDiskStack());
+        
+        foreach(GameObject disk in _diskHolder.GetDiskStack())
+        {
+            Debug.Log($"This Disk: {disk}");
+        }
+
+        foreach(GameObject disk in DiskSpawnManager.Instance.GetDiskStack())
+        {
+            Debug.Log($"Spawner's Disk: {disk}");
+        }
+
         if (isStackEqual)
         {
+            //Add game over logic here
             Debug.Log("TRUE");
+        }
+        else
+        {
+            Debug.Log("Stack is not equal. Keep Trying");
         }
     }
 
-    public void SetSelectedOverlay()
+    public DiskHolder GetDiskHolder()
     {
-        _selectedOverlay.gameObject.SetActive(true);
-    }
-
-    public void ResetSelectedOverlay()
-    {
-        _selectedOverlay.gameObject.SetActive(false);
+        return _diskHolder;
     }
 }
--- Assets/Scripts/KitchenObject/DiskHolder.cs
+++ Assets/Scripts/KitchenObject/DiskHolder.cs
@@ -1,28 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class DiskHolder : MonoBehaviour
 {
+    [SerializeField]private List<GameObject> _diskList = new();
     private Stack<GameObject> _diskStack = new();
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        _diskStack.Clear();
+        _diskList.Clear();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PushDisk(GameObject disk)
     {
-        
+        _diskList.Add(disk);
+        _diskStack.Push(disk);
+    }
+
+    public GameObject PopDisk()
+    {
+        if (_diskStack.Count > 0)
+        {
+            GameObject disk = _diskStack.Pop();
+            List<GameObject> poppedList = _diskList.ToList();
+            poppedList.Remove(disk);
+            _diskList = poppedList;
+            return disk;
+        }
+        else
+        {
+            return null;
+        }
     }
 
 
+    public GameObject GetLastDiskOnStack()
+    {
+        if (_diskStack != null && _diskStack.Count > 0)
+        {
+            return _diskStack.Peek();
+        }
+        return null;
+    }
 
+    public GameObject GetPoppedDisk()
+    {
+        if (_diskStack.Count > 0)
+        {
+            GameObject disk = _diskStack.Pop();
+            _diskList.Remove(disk);
+            return disk;
+        }
+        else
+        {
+            return null;
+        }
+    }
 
     public Stack<GameObject> GetDiskStack()
     {
-        return _diskStack;
+        return new(_diskStack);
+    }
+
+    public void SetDiskStack(Stack<GameObject> diskStack)
+    {
+        _diskList = new(diskStack);
+        _diskStack = diskStack;
     }
 }
